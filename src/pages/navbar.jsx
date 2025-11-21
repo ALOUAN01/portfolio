4b55084249b5f618d7d7fbe10814c368ee685d29
ALOUAN01 <alouanayoub3@gmail.com>
2025-11-21 23:33:38 +0100

    cv commit

--- src/pages/navbar.jsx
+++ src/pages/navbar.jsx
@@ -70,7 +70,7 @@ export default function Navbar() {
       <nav
         className={`fixed top-0 left-0 right-0 z-50 transition-all duration-300 ${
           isScrolled
-            ? " backdrop-blur-xl shadow-2xl border-b border-slate-800"
+            ? "backdrop-blur-xl shadow-2xl "
             : "bg-transparent"
         }`}
       >
